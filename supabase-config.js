edace544e99e5d75e9d8a6c5b7973f21ae5999eb
youngseonYU <u0seon@naver.com>
2026-07-28 16:34:24 +0900

    Add supabase-config.js

---
+++ supabase-config.js
@@ -0,0 +1,6 @@
+// Supabase publishable key??釉뚮씪?곗? ?대씪?댁뼵?몄뿉 ?몄텧?대룄 ?섎뒗 怨듦컻 ?ㅼ엯?덈떎.
+window.RECORDGYEOL_SUPABASE = {
+  url: 'https://hwpyrvkosspwdffadoee.supabase.co',
+  key: 'sb_publishable_Hg_7DCx05FTYhf6B_ONcxQ_L1QKGIa6'
+};
+
